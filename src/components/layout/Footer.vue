71e42580810a4d43a482dd5b80c7bcd98367c51a
mtyukala <mkhululi.tyukala@gmail.com>
2019-09-10 17:55:18 +0200

    Created Vuejs  Project

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer>
+        Vending Machine Demo 2019
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style>
+    footer {
+        background: rgba(0, 0, 0, .8);
+        color: #FFF;
+        position: fixed;
+        bottom: 0;
+        display: block;
+        height: 100px;
+        padding: 1em;
+        width: 100%;
+        text-align: center;
+    }
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <header class="header">
+        <nav id="nav">
+            <router-link to="/">Home</router-link>
+            |
+            <router-link to="/products">Products</router-link>
+            |
+            <router-link to="/coins">Coins</router-link>
+            |
+            <router-link to="/about">About</router-link>
+        </nav>
+        <h2>Vending Machine Demo</h2>
+    </header>
+</template>
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+<style scoped>
+    header {
+        text-align: center;
+    }
+
+    header a {
+    }
+</style>
